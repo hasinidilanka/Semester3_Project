d27b1ae793d6cfc37ccbebd53342741b32a789ee
sandaru <sandaru.14@cse.mrt.ac.lk>
2016-05-10 08:51:08 +0530

    getting the batch ID method

--- src/Controller/FieldController.java
+++ src/Controller/FieldController.java
@@ -5,10 +5,24 @@
  */
 package Controller;
 
+import DB.DBConnection;
+import DB.DBHandler;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Hasini
  */
 public class FieldController {
+    public static String searchFieldID(String field) throws SQLException, ClassNotFoundException{
+        String sql2="Select fieldID from Field where name='"+field+"'";
+       ResultSet rst2= DBHandler.getData(DBConnection.getDBConnection().getConnection(), sql2);
+       String fieldID="";
+       while(rst2.next()){
+           fieldID=rst2.getString(1);
+       }
+       return fieldID;
     
+    }
 }
