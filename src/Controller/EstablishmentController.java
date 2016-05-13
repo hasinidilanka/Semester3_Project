e0ea5ad3270b90e7687ec1e087bb921c2b85c1ef
rangika <rangika.14@cse.mrt.ac.lk>
2016-05-13 23:53:25 +0530

    In establishment controller add getFromDate method and setToDate method

--- src/Controller/EstablishmentController.java
+++ src/Controller/EstablishmentController.java
@@ -7,8 +7,13 @@ package Controller;
 
 import DB.DBConnection;
 import DB.DBHandler;
+import Model.Establishment;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
@@ -16,18 +21,51 @@ import java.sql.SQLException;
  */
 public class EstablishmentController {
     
-    public static String getEstablishment(String indexNo,int number) throws SQLException, ClassNotFoundException{
-        String sql = "Select companyID From Establishment where indexNumber='" +indexNo+ "' and number='"+number+"'";
+    public static String getEstablishmentID(String indexNo, int number) throws ClassNotFoundException, SQLException{
+        String sql = "Select establishmentID From Establishment where indexNumber='" + indexNo + "' and number='" + number + "'";
         ResultSet rst = DBHandler.getData(DBConnection.getDBConnection().getConnection(), sql);
-  
+
         while (rst.next()) {
-            
-            if(rst.getString(1).equals("")){
+            return rst.getString(1);
+        }
+        return null;
+    }
+
+    public static String getEstablishment(String indexNo, int number) throws SQLException, ClassNotFoundException {
+        String sql = "Select companyID From Establishment where indexNumber='" + indexNo + "' and number='" + number + "'";
+        ResultSet rst = DBHandler.getData(DBConnection.getDBConnection().getConnection(), sql);
+
+        while (rst.next()) {
+
+            if (rst.getString(1).equals("")) {
                 return null;
             }
             return rst.getString(1);
         }
         return null;
     }
-    
+
+    public static String getFromDate(String indexNo, int number) throws SQLException, ClassNotFoundException {
+        String sql = "Select fromDate From Establishment where indexNumber='" + indexNo + "' and number='" + number + "'";
+        ResultSet rst = DBHandler.getData(DBConnection.getDBConnection().getConnection(), sql);
+
+        while (rst.next()) {
+
+            if (rst.getString(1).equals("")) {
+                return null;
+            }
+            return rst.getString(1);
+        }
+        return null;
+    }
+
+    public static int setEndDate(Date endDate,String establishmentID) throws ClassNotFoundException, SQLException {
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Object today = new java.sql.Timestamp(endDate.getTime());
+        String datestring = dateFormat.format(today);
+        String sql = "Update Establishment SET toDate='"+datestring+"' where establishmentID='"+establishmentID+"'";
+        return DBHandler.setData(DBConnection.getDBConnection().getConnection(), sql);
+    }
+
 }
